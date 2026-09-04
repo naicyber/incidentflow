0ae990e7bed5c1942f8437fb0d5fedc53ccdd19d
naicyber <282132545+naicyber@users.noreply.github.com>
2026-09-04 15:38:42 -0300

    feat: persist incidents with PostgreSQL

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
+const pool = require("./db");
+
 const app = express();
 app.use(express.json());
 const PORT = 3000;
 
-let incidentCounter = 1;
-
-app.post("/incidents", (req, res) => {
+app.post("/incidents", async (req, res) => {
   console.log(req.body);
 
   const { title, severity, ip, user } = req.body;
+  
+ 
 
   if (!title || !severity || !ip || !user) {
     return res.status(400).json({
@@ -16,10 +18,30 @@ app.post("/incidents", (req, res) => {
     });
   }
 
-  const formattedNumber = String(incidentCounter).padStart(4, "0");
+  const idResult = await pool.query(
+    "SELECT nextval('incidents_id_seq') AS id"
+  );
+
+  const nextId = idResult.rows[0].id;
+  const formattedNumber = String(nextId).padStart(4, "0");
   const incidentId = `INC-2026-${formattedNumber}`;
 
-  incidentCounter++;
+  const query = `
+  INSERT INTO incidents
+  (id, incident_code, title, severity, ip, username)
+  VALUES ($1, $2, $3, $4, $5, $6)
+`;
+
+  const values = [
+  nextId,
+  incidentId,
+  title,
+  severity,
+  ip,
+  user
+];
+
+  await pool.query(query, values);
 
   console.log(incidentId);
 
@@ -28,6 +50,26 @@ app.post("/incidents", (req, res) => {
   status: "created",
   severity: severity
 });
+});
+
+app.get("/incidents", async (req, res) => {
+    const result = await pool.query(
+        "SELECT * FROM incidents ORDER BY id ASC");
+
+    res.status(200).json(result.rows);
+
+});
+
+app.get("/incidents/:id", async (req, res) => {
+    const { id } = req.params;
+    
+    const result = await pool.query(
+        "SELECT * FROM incidents WHERE id = $1",
+        [id]
+    );
+
+    res.status(200).json(result.rows[0]);
+    
 });
 
 app.listen(PORT, () => {
